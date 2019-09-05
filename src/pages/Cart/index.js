a31f8b641dc51efd4623aa2ff97b93b8d29e550e
Victor Rodrigues <victor.rodrigues@justdigital.com.br>
2019-09-05 15:57:34 -0300

    + Transform in class components

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
 // import { Container } from './styles';
 
-export default function Cart() {
-  return (
-    <View>
-      <Text>Cart</Text>
-    </View>
-  );
+export default class Cart extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Cart</Text>
+      </View>
+    );
+  }
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
 // import { Container } from './styles';
 
-export default function Home() {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  );
+export default class Home extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Home</Text>
+      </View>
+    );
+  }
 }
